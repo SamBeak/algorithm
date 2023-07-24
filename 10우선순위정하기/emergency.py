bb082b682aa394dbd22ec7263fb8a884bab10c3e
Baek HyunGyu <baekhg102@gmail.com>
2023-07-24 16:46:43 +0900

    docs: Add new algo

---
+++ 10우선순위정하기/emergency.py
@@ -0,0 +1,11 @@
+"""
+문제
+
+외과의사 머쓱이는 응급실에 온 환자의 응급도를 기준으로 진료 순서를 정하려고 합니다. 정수 배열 emergency가 매개변수로 주어질 때 응급도가 높은 순서대로 진료 순서를 정한 배열을 return하도록 solution 함수를 완성해주세요.
+"""
+
+def solution(emergency):
+    # copy = sorted(emergency, reverse=True)
+    # for i in emergency:
+    #     print(copy.index(i)+1, end="")
+    return list(map(lambda value: sorted(emergency, reverse=True).index(value)+1, emergency))
